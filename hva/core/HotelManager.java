8d2025db43c4d51bb614dc30364e9ec37688279e
Duarte Cruz <duartecruz@MacBookPro.lan>
2024-10-11 15:21:59 +0100

    added comments on HotelManager class. Also made some changes in there. Only missing comments on Hotel class

--- hva/core/HotelManager.java
+++ hva/core/HotelManager.java
@@ -3,8 +3,6 @@ package hva.core;
 import hva.core.exception.*;
 import java.io.*;
 
-// FIXME import classes
-
 /**
  * Class representing the manager of this application. It manages the current
  * zoo hotel.
@@ -15,6 +13,7 @@ public class HotelManager{
   private String _path;
 
   public HotelManager(){
+    //initializes a new hotel
     _hotel = new Hotel();
   }
   /**
@@ -25,14 +24,24 @@ public class HotelManager{
    * @throws IOException if there is some error while serializing the state of the network to disk.
    **/
   public void save() throws FileNotFoundException, MissingFileAssociationException, IOException {
-    if(_hotel.houveAlteracoes()){
+    //saves the current hotel in the current file
+
+    //checks if there were any changes
+    if(houveAlteracoes()){
+      //there were changes, so we need to save them
       try {
+        //saves to the current path
         saveAs(_path);
+        //changes the _alteracoes boolean on hotel, because we saved the changes that were made
+        //therefore there are no new changes
         _hotel.changeAlteracoes();
+
       } catch(UnavailableFileException| NullPointerException | MissingFileAssociationException | IOException e){
+        //if there is an exception, we have to throw the MissingFileAssociationException 
         throw new MissingFileAssociationException();
       } 
     }
+    //there were no changes, so we don't need to save
   }
   
   /**
@@ -46,15 +55,25 @@ public class HotelManager{
    * @throws UnavailableFileException 
    **/
   public void saveAs(String filename) throws FileNotFoundException, MissingFileAssociationException, IOException, UnavailableFileException {
-    if(_hotel.houveAlteracoes()){
+    //saves the current hotel on a newly created file whose name is the value of the variable filename
+
+    //checks if there were any changes
+    if(houveAlteracoes()){
+      //there were changes, so we need to save them
       try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+        //tries to save the current hotel to a new file whose name is the value of the variable filename
         oos.writeObject(_hotel);
+        //saves the new path on the variable _path
         _path = filename;
+        //changes the _alteracoes boolean on hotel, because we saved the changes that were made
+        //therefore there are no new changes
         _hotel.changeAlteracoes();
       } catch(IOException e){
+        //if there is an exception, we have to throw the UnavailableFileException 
         throw new UnavailableFileException(filename);
       } 
     }
+    //there were no changes, so we don't need to save
   }
   
   /**
@@ -64,13 +83,21 @@ public class HotelManager{
    *         an error while processing this file.
    **/
   public void load(String filename) throws UnavailableFileException {
+    //load an hotel in a file whose name is the value of the variable filename
+
     try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+      //tries to open the file and get its values
+      //saves the file's Hotel in _hotel
       _hotel = (Hotel)ois.readObject();
-      if(_hotel.houveAlteracoes()){
+      //checks if there were variable _alteracoes in this new Hotel, because we just saved this Hotel's changes
+      if(houveAlteracoes()){
+        //changes the _alteracoes boolean on hotel, because we saved the changes that were made
         _hotel.changeAlteracoes();
       }
+      //saves the new path on the variable _path
       _path = filename;
     } catch (IOException | ClassNotFoundException e) {
+      //if there is an exception, we have to throw the UnavailableFileException 
       throw new UnavailableFileException(filename);
     }
   }
@@ -98,9 +125,12 @@ public class HotelManager{
   AnimalJaExiste, VacinaJaExiste, 
    FuncionarioJaExiste, ArvoreJaExiste, NaoResponsabilidade, ArvoreNaoExiste, 
    HabitatNaoExiste, EspecieNaoExiste, FuncionarioNaoExiste {
+    //imports a file, whose name is the value of the variable filename, with values from an hotel
     try {
+      //tries to import the file whose name is the value of the variable filename
       _hotel.importFile(filename);
-    } catch (IOException | UnrecognizedEntryException /* FIXME maybe other exceptions */ e) {
+    } catch (IOException | UnrecognizedEntryException e) {
+      //if there is an exception, we have to throw the ImportFileException 
       throw new ImportFileException(filename, e);
     }
   }
@@ -111,14 +141,17 @@ public class HotelManager{
    * @return the current zoo hotel
    **/
   public final Hotel getHotel() {
+    //returns the current hotel
     return _hotel;
   }
 
   public boolean houveAlteracoes(){
+    //checks if there were any changes in the current hotel
     return _hotel.houveAlteracoes();
   }
 
   public void novoHotel(){
+    //creates a new hotel
     _hotel = new Hotel();
   }
 }
